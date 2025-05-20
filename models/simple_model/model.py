a210d21feb95ebe962f22933310d88d50a569a78
Arne van den Berg <a.berg@camous.lmu.de>
2025-05-20 16:29:57 +0200

    modified model

--- models/simple_model/model.py
+++ models/simple_model/model.py
@@ -1,42 +1,15 @@
-import torch
 import torch.nn as nn
 
-
-import model_parts as mp
+from models.simple_model import model_parts as mp
 
 MAX_PROTEIN_LENGTH = 100
-N_PFAMS = 800
-
-PROTEIN_ALPHABET = {
-    '-': 0,
-    'A': 1,
-    'C': 2,
-    'D': 3,
-    'E': 4,
-    'F': 5,
-    'G': 6,
-    'H': 7,
-    'I': 8,
-    'K': 9,
-    'L': 10,
-    'M': 11,
-    'N': 12,
-    'P': 13,
-    'Q': 14,
-    'R': 15,
-    'S': 16,
-    'T': 17,
-    'V': 18,
-    'W': 19,
-    'Y': 20,
-}
 
 # Here is the original code for the ProtENN2 model in tensorflow:
 # https://github.com/iponamareva/protcnn/blob/main/layers.py
 
-class ProtENN2_one_hot(nn.Module):
+class ProtENN2_style(nn.Module):
     
-    def __init__(self, cnn_dim=128, in_channels=21):    # either 21 for one-hot input or 1024 for ProtT5 input
+    def __init__(self, cnn_dim=128, in_channels=21, num_pfams=100):    # either 21 for one-hot input or 1024 for ProtT5 input
         super().__init__()
         
         # Input shape: (batch_size, MAX_PROTEIN_LENGTH, 21)
@@ -50,7 +23,7 @@ class ProtENN2_one_hot(nn.Module):
         self.cnn_out = nn.Conv1d(in_channels=cnn_dim, out_channels=1100, kernel_size=3, padding=1)
         # Output shape: (batch_size, 1100, MAX_PROTEIN_LENGTH)
 
-        self.final_linear = nn.Linear(1100, N_PFAMS)
+        self.final_linear = nn.Linear(1100, num_pfams)
         # self.softmax = nn.Softmax(dim=-1)
         self.relu = nn.ReLU()
 
@@ -74,4 +47,3 @@ class ProtENN2_one_hot(nn.Module):
         
         return x
         # return self.softmax(x)    # not needed when using CrossEntropyLoss
-
